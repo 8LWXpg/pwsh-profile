29b950336b5b470ac785bb1062fb8b4bf0d49737
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-10-29 22:34:09 +0800

    remove transient prompt

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,11 @@
 # prevent loading profile in scripts
 $temp = ([System.Environment]::GetCommandLineArgs())
 if ($temp.Count -gt 1 -and
-    $temp -notcontains '-NoExit' -and
-    $temp -notcontains '-NoLogo' -and
-    $temp -notcontains '-noe' -and
-    $temp -notcontains '-nol') {
-    return
+	$temp -notcontains '-NoExit' -and
+	$temp -notcontains '-NoLogo' -and
+	$temp -notcontains '-noe' -and
+	$temp -notcontains '-nol') {
+	return
 }
 Remove-Variable temp
 
@@ -16,9 +16,9 @@ Import-Module PSFzf
 
 Set-PSReadLineOption -PredictionSource History -EditMode Windows -HistoryNoDuplicates
 if ($Host.UI.RawUI.WindowSize.Height -lt 15 -or $Host.UI.RawUI.WindowSize.Width -lt 54) {
-    Set-PSReadLineOption -PredictionViewStyle InlineView
+	Set-PSReadLineOption -PredictionViewStyle InlineView
 } else {
-    Set-PSReadLineOption -PredictionViewStyle ListView
+	Set-PSReadLineOption -PredictionViewStyle ListView
 }
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory F7 -TabExpansion
 
@@ -28,11 +28,5 @@ $ProfileFolder = 'E:\ps1\profile'
 . "$ProfileFolder\keys.ps1"
 . "$ProfileFolder\scripts.ps1"
 . "$ProfileFolder\complete.ps1"
-
-$__transientPrompt = starship module character
-function Invoke-Starship-TransientFunction {
-    $__transientPrompt
-}
 . "$ProfileFolder\starship.ps1"
-Enable-TransientPrompt
 Remove-Variable ProfileFolder
