1dc1d1f3e55d301cedf1d980cb3355bad4b01162
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-11-29 22:26:02 +0800

    fix dotnet complete

--- complete.ps1
+++ complete.ps1
@@ -12,8 +12,8 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
 #region dotnet
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 	param($wordToComplete, $commandAst, $cursorPosition)
-	dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
-		[System.Management.Automation.CompletionResult]::new($_, $_, $_)
+	dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
 	}
 }
 #endregion
