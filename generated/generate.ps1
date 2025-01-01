de1d228570ec2cfea6a0a87a08dab92dcdc0f357
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-01-01 19:41:29 +0800

    add uv

--- generated/generate.ps1
+++ generated/generate.ps1
@@ -6,5 +6,6 @@ Push-Location
 gh completion -s powershell > gh.ps1
 zoxide init powershell > zoxide.ps1
 starship init powershell --print-full-init > starship.ps1
+uv generate-shell-completion powershell > uv.ps1
 
 Pop-Location
