3ee2d86b6bccd6a9faa1c8b12dd60ef83f00b09e
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-01-17 22:34:38 +0800

    add sudo helper function

--- scripts.ps1
+++ scripts.ps1
@@ -18,9 +18,36 @@ function yy {
 	Remove-Item -Path $tmp
 }
 
-# uv with system
-function uvs {
-	uv $args --system
+# sudo
+function sudo {
+	begin {
+		function convertToBase64EncodedString([string]$cmdLine) {
+			[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cmdLine))
+		}
+	}
+	end {
+		switch ($args[0]) {
+			{ $_ -is [scriptblock] } {
+				$encoded = convertToBase64EncodedString $_
+				sudo.exe pwsh -e $encoded
+			}
+			{ Get-Command $_ -Type Application -ErrorAction Ignore } {
+				# pass as-is for native command
+				sudo.exe $args
+			}
+			{ Get-Command $_ -Type Cmdlet, ExternalScript, Alias -ErrorAction Ignore } {
+				$encoded = convertToBase64EncodedString "$args"
+				sudo.exe pwsh -e $encoded
+			}
+			'!!' {
+				$encoded = convertToBase64EncodedString "$(Get-History -c 1)"
+				sudo.exe pwsh -e $encoded
+			}
+			default {
+				throw "Cannot find '$_'"
+			}
+		}
+	}
 }
 
 function .. { Set-Location .. }
