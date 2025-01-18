352ae8d063ac9eaa72d7a67d897f432ea8f76aa5
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-01-18 13:02:33 +0800

    load sudo in script

--- scripts.ps1
+++ scripts.ps1
@@ -18,46 +18,6 @@ function yy {
 	Remove-Item -Path $tmp
 }
 
-# sudo
-function sudo {
-	begin {
-		function convertToBase64EncodedString([string]$cmdLine) {
-			[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cmdLine))
-		}
-	}
-	end {
-		switch ($args[0]) {
-			$null {
-				sudo.exe pwsh -nol
-				break
-			}
-			'!!' {
-				$encoded = convertToBase64EncodedString "$(Get-History -c 1)"
-				sudo.exe pwsh -e $encoded
-				break
-			}
-			{ $_ -is [scriptblock] } {
-				$encoded = convertToBase64EncodedString $_
-				sudo.exe pwsh -e $encoded
-				break
-			}
-			{ Get-Command $_ -Type Application -ErrorAction Ignore } {
-				# pass as-is for native command
-				sudo.exe $args
-				break
-			}
-			{ Get-Command $_ -Type Cmdlet, ExternalScript, Alias -ErrorAction Ignore } {
-				$encoded = convertToBase64EncodedString "$args"
-				sudo.exe pwsh -e $encoded
-				break
-			}
-			default {
-				throw "Cannot find '$_'"
-			}
-		}
-	}
-}
-
 function .. { Set-Location .. }
 function ... { Set-Location ..\.. }
 function .... { Set-Location ..\..\.. }
